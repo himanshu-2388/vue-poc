2f01a7136fc8c885483c59fbb5ec4b3364601842
Himanshu Saxena <himanshu2388@gmail.com>
2021-03-09 02:19:03 +0530

    Code commit for access specifers

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>Halo DIL POC</h3>
+    <div>
+       <label> Public Variable </label>
+       <span> Value {{publicVariableValue}} </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  private publicVariableValue: string;
+
+  public constructor() {
+    super();
+    this.publicVariableValue = "New Pubic Variable";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
